f0cf7b4379bac756740df98d89fcf99bd5642e7e
Raghav <100552980+Rgulati4657@users.noreply.github.com>
2023-10-29 10:39:39 +0530

    Add files via upload

---
+++ studentreg/studentreg/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace studentreg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            txtFee = new TextBox();
+            txtCourse = new TextBox();
+            txtName = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            EDIT = new DataGridViewButtonColumn();
+            DELETE = new DataGridViewButtonColumn();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tempus Sans ITC", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(364, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(418, 57);
+            label1.TabIndex = 0;
+            label1.Text = "Student Registration";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtFee);
+            groupBox1.Controls.Add(txtCourse);
+            groupBox1.Controls.Add(txtName);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 126);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(437, 246);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Student Block";
+            // 
+            // txtFee
+            // 
+            txtFee.Location = new Point(163, 170);
+            txtFee.Name = "txtFee";
+            txtFee.Size = new Size(251, 27);
+            txtFee.TabIndex = 5;
+            // 
+            // txtCourse
+            // 
+            txtCourse.Location = new Point(163, 116);
+            txtCourse.Name = "txtCourse";
+            txtCourse.Size = new Size(251, 27);
+            txtCourse.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(163, 61);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(251, 27);
+            txtName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(29, 170);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 24);
+            label4.TabIndex = 2;
+            label4.Text = "Fee";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(19, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 24);
+            label3.TabIndex = 1;
+            label3.Text = "Course";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(19, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 24);
+            label2.TabIndex = 0;
+            label2.Text = "Student Name";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 392);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 46);
+            button1.TabIndex = 2;
+            button1.Text = "SAVE";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(185, 392);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 46);
+            button2.TabIndex = 3;
+            button2.Text = "CLEAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, EDIT, DELETE });
+            dataGridView1.Location = new Point(455, 109);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(804, 346);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "ID";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Student Name";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Course";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Fee";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.Width = 125;
+            // 
+            // EDIT
+            // 
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            EDIT.DefaultCellStyle = dataGridViewCellStyle1;
+            EDIT.HeaderText = "EDIT";
+            EDIT.MinimumWidth = 6;
+            EDIT.Name = "EDIT";
+            EDIT.Text = "EDIT";
+            EDIT.UseColumnTextForButtonValue = true;
+            EDIT.Width = 125;
+            // 
+            // DELETE
+            // 
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            DELETE.DefaultCellStyle = dataGridViewCellStyle2;
+            DELETE.HeaderText = "DELETE";
+            DELETE.MinimumWidth = 6;
+            DELETE.Name = "DELETE";
+            DELETE.Text = "DELETE";
+            DELETE.UseColumnTextForButtonValue = true;
+            DELETE.Width = 125;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(909, 477);
+            button3.Name = "button3";
+            button3.Size = new Size(302, 29);
+            button3.TabIndex = 5;
+            button3.Text = "REFRESH";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1271, 549);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox txtFee;
+        private TextBox txtCourse;
+        private TextBox txtName;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewButtonColumn EDIT;
+        private DataGridViewButtonColumn DELETE;
+        private Button button3;
+    }
+}
---
+++ studentreg/studentreg/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System.Data.SqlClient;
+
+namespace studentreg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load();
+
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source = (localdb)\\Local; Initial Catalog = mydb; Integrated Security = True");
+        SqlCommand cmd;
+        SqlDataReader read;
+        string id;
+        bool Mode = true;
+        string sql;
+        SqlDataAdapter adapter;
+        public void Load()
+        {
+            try
+            {
+                sql = "select * from student";
+                cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                read = cmd.ExecuteReader();
+                // adapter = new SqlDataAdapter(sql,conn);
+                dataGridView1.Rows.Clear();
+
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(read[0], read[1], read[2], read[3]);
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void getID(string id)
+        {
+            sql = "select * from  student where id = '" + id + "'";
+            cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            read = cmd.ExecuteReader();
+
+            while (read.Read())
+            {
+                txtName.Text = read[1].ToString();
+                txtCourse.Text = read[2].ToString();
+                txtFee.Text = read[3].ToString();
+
+            }
+
+            conn.Close();
+        }
+
+
+        // if the mode is true means add record otherwise update record;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string course = txtCourse.Text;
+            string fee = txtFee.Text;
+
+            if (Mode == true)
+            {
+                sql = "insert into student (stname,course,fee) values (@stname,@course,@fee)";
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                MessageBox.Show("Record Added \\(>.<)/ ");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+
+                txtName.Focus();
+
+
+
+            }
+            else
+            {
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "update student set stname = @stname, course = @course, fee = @fee where id = @id ";
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                cmd.Parameters.AddWithValue("@id", id);
+                MessageBox.Show("Record Updated (～￣▽￣)～ ");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+
+                txtName.Focus();
+                button1.Text = "SAVE";
+                Mode = true;
+
+            }
+            conn.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["EDIT"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                getID(id);
+                button1.Text = "UPDATE";
+            }
+            if (e.ColumnIndex == dataGridView1.Columns["DELETE"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "delete from student where id = @id ";
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Deleted ✍️(◔◡◔)");
+                conn.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Load();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            txtName.Clear();
+            txtCourse.Clear();
+            txtFee.Clear();
+
+            txtName.Focus();
+            button1.Text = "SAVE";
+            Mode = true;
+
+        }
+    }
+}
