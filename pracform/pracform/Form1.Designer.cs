b9881475ecc4b8e9e67d5855b736c9ffee63eafe
Raghav <100552980+Rgulati4657@users.noreply.github.com>
2023-10-29 10:36:20 +0530

    Add files via upload

---
+++ pracform/pracform/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace pracform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtb1 = new TextBox();
+            txtb2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(168, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 20);
+            label1.TabIndex = 0;
+            label1.Text = "USER NAME";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(168, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 20);
+            label2.TabIndex = 1;
+            label2.Text = "PASSWORD";
+            // 
+            // txtb1
+            // 
+            txtb1.Location = new Point(282, 92);
+            txtb1.Name = "txtb1";
+            txtb1.PlaceholderText = "USERNAME";
+            txtb1.Size = new Size(210, 27);
+            txtb1.TabIndex = 2;
+            // 
+            // txtb2
+            // 
+            txtb2.Location = new Point(282, 153);
+            txtb2.Name = "txtb2";
+            txtb2.PasswordChar = '*';
+            txtb2.PlaceholderText = "*********";
+            txtb2.Size = new Size(210, 27);
+            txtb2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(314, 219);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 33);
+            button1.TabIndex = 4;
+            button1.Text = "SUBMIT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(txtb2);
+            Controls.Add(txtb1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtb1;
+        private TextBox txtb2;
+        private Button button1;
+    }
+}
